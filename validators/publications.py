e22cf98be4ff176e2cdc8cce53afb6f1974da02b
AleYK <100762788+Cykallu@users.noreply.github.com>
2022-04-26 12:32:26 +0300

    Add files via upload

---
+++ validators/publications.py
@@ -0,0 +1,32 @@
+from errors.unauthorized import Unauthorized
+from flask import  jsonify, request
+
+def validate_publication_route_handler(publication_route_handler):
+    def validate_update_publication_wrapper(*args, **kwargs):
+        if request.method == 'GET':
+            return publication_route_handler(*args, **kwargs)
+        elif request.method == 'PATCH':
+            logged_in_user = get_jwt()
+            if not logged_in_user:
+                raise Unauthorized()
+
+            request_body = request.get_json()
+            if not request_body:
+                raise ValidationError('request body is required')
+            if 'title' not in request_body:
+                raise ValidationError('title is required')
+            if 'description' not in request_body:
+                raise ValidationError('description is required')
+            if 'visibility' not in request_body:
+                raise ValidationError('visibility is required')
+            return publication_route_handler(*args, **kwargs)
+        
+        elif request.method == 'DELETE':
+            logged_in_user = get_jwt
+            if not logged_in_user:
+                raise Unauthorized()
+            else:
+                return publication_route_handler(*args, **kwargs)
+    return validate_update_publication_wrapper
+
+
