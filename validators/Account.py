354877144d2cb35efe22f77e3fede2b3231d4ff4
AleYK <100762788+Cykallu@users.noreply.github.com>
2022-04-26 11:33:31 +0300

    update 26.4.2022

--- validators/Account.py
+++ validators/Account.py
@@ -21,4 +21,44 @@ def validate_account(account_route_handler_func):
                 # tänne mennään, jos request bodya ei ole edes lähetetty (vasen sarake insomniassa)
                 raise ValidationError('Body is required')
     return validate_account_wrapper
+
+def validate_account_password_route_handler(f):
+    def validate_account_password_route_handler_wrapper(*args,**kwargs):
+        request_body = request.get_json()
+        if request.method == 'PATCH':
+            if request_body:
+                if 'password' in request_body:
+                    return validate_account_password_route_handler(*args, **kwargs)
+        else:
+            return f(*args, **kwargs)
+    return validate_account_password_route_handler_wrapper
+
+
+def validate_update_profile_picture(func):
+    def validate_update_profile_picture_wrapper(*args, **kwargs):
+        request_body = request.get_json()
+        if request.method == 'PATCH':
+            if request_body:
+                if 'profile_picture' in request_body:
+                    return validate_update_profile_picture(*args, **kwargs)
+        else:
+            return func(*args, **kwargs)
+    return validate_update_profile_picture_wrapper
+
+def check_profile_picture_max_size(max_size_in_kt = 100):
+    def check_profile_picture_max_size_wrapper(func):
+        def wrapper(*args, **kwargs):
+            request_body = request.get_json()
+            profile_pic = request_body['profile_picture']
+            pic_to_bytes = (len(profile_pic)*3)/4
+            size_in_kt = pic_to_bytes/1024
+            if request_body:
+                if 'profile_picture' in request_body:
+                    if size_in_kt <= max_size_in_kt:
+                        return func(*args, **kwargs)
+        return wrapper
+    return check_profile_picture_max_size_wrapper                    
+
+
+
         
